ab2bac982fc6170388d9fa7ca051e90095f8da64
Michal Wojciechowski <odyniec@odyniec.net>
2013-02-17 17:09:07 +0100

    Add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('selectlist.jquery.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= pkg.version %> */ '
+            },
+            dist: {
+                src: 'jquery.<%= pkg.name %>.dev.js',
+                dest: 'dist/jquery.<%= pkg.name %>-<%= pkg.version %>/scripts/jquery.<%= pkg.name %>.min.js'
+            }
+        },
+        copy: {
+            dist: {
+                files: [
+                    {
+                        expand: true, cwd: 'distfiles/', src: [ '**' ],
+                        dest: 'dist/jquery.<%= pkg.name %>-<%= pkg.version %>/'
+                    }
+                ]
+            }
+        },
+        qunit: {
+            all: [ 'test/*.html' ]
+        },
+        compress: {
+            dist: {
+                options: {
+                    archive: 'dist/jquery.<%= pkg.name %>-<%= pkg.version %>.zip'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'dist/',
+                    src: 'jquery.<%= pkg.name %>-<%= pkg.version %>/**',
+                    dest: ''
+                }]
+            }
+        }
+    });
+
+    // Load the plugins
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default tasks
+    grunt.registerTask('default', [ 'uglify', 'copy' ]);
+};
+
