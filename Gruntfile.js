4da9200f6bb57ed7cca8969db2eb8450ac24c0b2
Michal Wojciechowski <odyniec@odyniec.net>
2013-02-27 20:06:02 +0100

    Generate uglified files in the root directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
                     banner: '/*! <%= pkg.name %> <%= pkg.version %> */ '
                 },
                 files: {
-                    'dist/jquery.<%= pkg.name %>-<%= pkg.version %>/scripts/jquery.<%= pkg.name %>.min.js':
+                    'jquery.<%= pkg.name %>.min.js':
                         [ 'jquery.<%= pkg.name %>.dev.js' ]
                 }
             },
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
                     preserveComments: 'some'
                 },
                 files: {
-                    'dist/jquery.<%= pkg.name %>-<%= pkg.version %>/scripts/jquery.<%= pkg.name %>.js':
+                    'jquery.<%= pkg.name %>.js':
                         [ 'jquery.<%= pkg.name %>.dev.js' ]
                 }
             }
@@ -31,8 +31,8 @@ module.exports = function(grunt) {
                         dest: 'dist/jquery.<%= pkg.name %>-<%= pkg.version %>/'
                     },
                     {
-                        'dist/jquery.<%= pkg.name %>-<%= pkg.version %>/scripts/jquery.<%= pkg.name %>.dev.js':
-                            [ 'jquery.<%= pkg.name %>.dev.js' ]
+                        src: [ 'jquery.<%= pkg.name %>*.js' ],
+                        dest: 'dist/jquery.<%= pkg.name %>-<%= pkg.version %>/scripts/'
                     }
                 ]
             }
